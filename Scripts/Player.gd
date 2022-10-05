1f22d33d1ebce93a72f7f2f989d09b6bd9f3ff51
justAbhi77 <ojhaabhinav2016@gmail.com>
2022-10-05 13:10:09 +0530

    Animatons , Animation Trees, Animation States and Blend Spaces.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
+
 export (int) var speed = 200
 
 var velocity = Vector2()
+
+onready var animPlayer=$AnimationPlayer
+onready var animTree=$AnimationTree
+onready var animState=animTree.get("parameters/playback")
 
 func _process(delta):
 	velocity = Vector2()
@@ -15,6 +20,13 @@ func _process(delta):
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
-
+	
+	if velocity != Vector2.ZERO:
+		animTree.set("parameters/Run/blend_position", velocity)
+		animTree.set("parameters/Idle/blend_position", velocity)
+		animState.travel("Run")
+	else:
+		animState.travel("Idle")
+		
 func _physics_process(delta):
 	move_and_collide(velocity*delta)
